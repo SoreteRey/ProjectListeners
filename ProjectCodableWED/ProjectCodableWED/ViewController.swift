985b9d2230e0ed7e25378a58936ba5aadb800056
sguiscardo <sdguiscardo@gmail.com>
2023-03-01 12:20:50 -0600

    creating project

---
+++ ProjectCodableWED/ProjectCodableWED/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectCodableWED
+//
+//  Created by Sebastian Guiscardo on 3/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
